498b804e2bc2567dc8517bc626c41d2c5a949db1
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-08-12 17:08:31 -0300

    chore: initial Spring Boot skeleton

---
+++ src/main/java/com/rafael/monitoramento_precos/MonitoramentoPrecosApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.monitoramento_precos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoPrecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoPrecosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/monitoramento_precos/MonitoramentoPrecosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.monitoramento_precos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoPrecosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
