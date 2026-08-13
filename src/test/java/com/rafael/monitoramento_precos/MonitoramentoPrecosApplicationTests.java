92424322d6eb709c03d1e18733925800e6bdfe68
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-08-12 23:11:27 -0300

    test: disable database auto-configuration for contextLoads in CI

--- src/test/java/com/rafael/monitoramento_precos/MonitoramentoPrecosApplicationTests.java
+++ src/test/java/com/rafael/monitoramento_precos/MonitoramentoPrecosApplicationTests.java
@@ -1,9 +1,15 @@
-package com.rafael.monitoramento_precos;
+package com.rafael.monitoramentoprecos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=" +
+				"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
+})
 class MonitoramentoPrecosApplicationTests {
 
 	@Test
