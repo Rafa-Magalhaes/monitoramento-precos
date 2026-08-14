f516ee9f116d5cc496a64145924cdf73706ff5de
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-08-14 17:14:51 -0300

    feat(auth): implementa servicos de geracao de jwt, controller de login e validacao argon2id

---
+++ src/main/java/com/rafael/monitoramento_precos/domain/service/AuthService.java
@@ -0,0 +1,41 @@
+package com.rafael.monitoramento_precos.domain.service;
+
+import com.rafael.monitoramento_precos.api.dto.request.LoginRequestDTO;
+import com.rafael.monitoramento_precos.api.dto.response.LoginResponseDTO;
+import com.rafael.monitoramento_precos.domain.model.Usuario;
+import com.rafael.monitoramento_precos.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @Value("${api.security.pepper:MinhaChavePepperSecreta123!}")
+    private String pepper;
+
+    public LoginResponseDTO autenticar(LoginRequestDTO dto) {
+
+        Usuario usuario = usuarioRepository.findByEmail(dto.getEmail())
+                .orElseThrow(() -> new BadCredentialsException("E-mail ou senha inválidos."));
+
+        String senhaComPepper = dto.getSenha() + pepper;
+
+        if (!passwordEncoder.matches(senhaComPepper, usuario.getSenha())) {
+            throw new BadCredentialsException("E-mail ou senha inválidos.");
+        }
+
+        String token = tokenService.gerarToken(usuario);
+
+        return LoginResponseDTO.builder()
+                .token(token)
+                .build();
+    }
+}
