6daf04ef8263b9879ec6922f9a9b3f142c78b37b
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-08-13 23:35:05 -0300

    feat(usuario): implementa cadastro de usuario, criptografia argon2 e endpoint REST

---
+++ src/main/java/com/rafael/monitoramento_precos/api/dto/response/UsuarioResponseDTO.java
@@ -0,0 +1,16 @@
+package com.rafael.monitoramento_precos.api.dto.response;
+
+import com.rafael.monitoramento_precos.domain.enums.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+public class UsuarioResponseDTO {
+    private UUID id;
+    private String nome;
+    private String email;
+    private Role role;
+}
---
+++ src/main/java/com/rafael/monitoramento_precos/domain/service/UsuarioService.java
@@ -0,0 +1,39 @@
+package com.rafael.monitoramento_precos.domain.service;
+
+import com.rafael.monitoramento_precos.api.converter.UsuarioConverter;
+import com.rafael.monitoramento_precos.api.dto.request.UsuarioCreateRequestDTO;
+import com.rafael.monitoramento_precos.domain.exception.ConflictException;
+import com.rafael.monitoramento_precos.domain.model.Usuario;
+import com.rafael.monitoramento_precos.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UsuarioConverter usuarioConverter;
+
+    @Value("${api.security.pepper:MinhaChavePepperSecreta123!}")
+    private String pepper;
+
+    @Transactional
+    public Usuario criarUsuario(UsuarioCreateRequestDTO dto) {
+
+        if (usuarioRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new ConflictException("Já existe um usuário cadastrado com este e-mail.");
+        }
+
+        String senhaComPepper = dto.getSenha() + pepper;
+        String senhaHash = passwordEncoder.encode(senhaComPepper);
+
+        Usuario novoUsuario = usuarioConverter.toEntity(dto, senhaHash);
+
+        return usuarioRepository.save(novoUsuario);
+    }
+}
