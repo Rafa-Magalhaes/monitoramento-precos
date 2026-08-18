77a5ef0aee77db1ca436609d21326a35b9780344
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-08-18 00:04:09 -0300

    fix(test): adiciona propriedade api.scraperapi.key falsa no contexto de testes para corrigir build no CI/CD

--- src/test/java/com/rafael/monitoramento_precos/MonitoramentoPrecosApplicationTests.java
+++ src/test/java/com/rafael/monitoramento_precos/MonitoramentoPrecosApplicationTests.java
@@ -9,7 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 		"spring.datasource.password=admin",
 		"spring.data.mongodb.uri=mongodb://localhost:27018/monitoramento_db",
 		"api.security.token.secret=chave-falsa-jwt",
-		"api.security.pepper=pimenta-falsa-pepper"
+		"api.security.pepper=pimenta-falsa-pepper",
+		"api.scraperapi.key=chave-falsa-scraperapi-para-testes"
 })
 class MonitoramentoPrecosApplicationTests {
 
